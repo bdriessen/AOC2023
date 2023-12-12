7e032e087d34fe92817f3a9a80567dcbc139e1c8
Bart <bart.driessen@pm.me>
2023-12-12 22:21:52 +0100

    lastiger dan ik dacht...

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -26,6 +26,44 @@ def read_input(fn):
 def part1(fname):
     res = read_input(fname)
 
+    universe = np.array([list(line) for line in res])
+
+    ic(universe)
+    galaxies = []
+    for i in range(0, len(universe)):
+        for j in range(0, len(universe[0])):
+            if universe[i,j] == '#':
+                galaxies.append([i,j])
+
+    # Find rows and columns containing only '.'
+    rows = []
+    cols = []
+    for i in range(0, len(universe)):
+        if np.all(universe[i,:] == '.'):
+            rows.append(i)
+    for j in range(0, len(universe[0])):
+        if np.all(universe[:,j] == '.'):
+            cols.append(j)
+
+    ic(galaxies, rows, cols)
+
+    for row in rows:
+        # Shift all galaxies below row one down
+        galaxies = [[i+1,j] if i >= row else [i,j] for i,j in galaxies]
+
+    new_rows
+    for i in range(len(rows), 0, -1):
+        for j in range(i+1, len(rows)):
+            rows[j] += 1
+
+        rows = rows[:i] + [i] + rows[i:]
+
+    for i in range(len(cols)):
+        cols[i:] += 1
+        cols = cols[:i] + [i] + cols[i:]
+
+    ic(galaxies, rows, cols)
+
     return 0
 
 
@@ -55,13 +93,8 @@ def main():
         fname = "testinput.txt"
 
     if part == 1:
-        res1, visited = part1(fname)
-        nr_v=0
-        for i in range(len(visited)):
-            for j in range(len(visited[0])):
-                if visited[i][j]:
-                    nr_v += 1
-        print("Part 1: ", res1, nr_v)
+        res1 = part1(fname)
+        print("Part 1: ", res1)
     else:
         res2 = part2(fname)
         print("Part 2: ", res2)
