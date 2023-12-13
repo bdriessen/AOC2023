4be08fc46fb253cc58f0a473f329dbbef05697d5
Bart <bart.driessen@pm.me>
2023-12-13 09:23:10 +0100

    11b correct

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -21,7 +21,6 @@ def read_input(fn):
     return lines
 
 
-
 # Part 1
 def part1(fname):
     res = read_input(fname)
@@ -32,48 +31,52 @@ def part1(fname):
     galaxies = []
     for i in range(0, len(universe)):
         for j in range(0, len(universe[0])):
-            if universe[i,j] == '#':
-                galaxies.append([i,j])
+            if universe[i, j] == '#':
+                galaxies.append([i, j])
 
     # Find rows and columns containing only '.'
     rows = []
     cols = []
     for i in range(0, len(universe)):
-        if np.all(universe[i,:] == '.'):
+        if np.all(universe[i, :] == '.'):
             rows.append(i)
     for j in range(0, len(universe[0])):
-        if np.all(universe[:,j] == '.'):
+        if np.all(universe[:, j] == '.'):
             cols.append(j)
 
     ic(galaxies, rows, cols)
 
-
-    for i in range(len(rows)-1, -1, -1):
+    for i in range(len(rows) - 1, -1, -1):
         # Shift all galaxies down
         for galaxy in galaxies:
             if galaxy[0] > rows[i]:
                 galaxy[0] += 1
-        rows = rows[:i] + [rows[i]] + [r+1 for r in rows[i:]]
+        rows = rows[:i] + [rows[i]] + [r + 1 for r in rows[i:]]
 
-    for j in range(len(cols)-1, -1, -1):
+    for j in range(len(cols) - 1, -1, -1):
         # Shift all galaxies right
         for galaxy in galaxies:
             if galaxy[1] > cols[j]:
                 galaxy[1] += 1
-        cols = cols[:j] + [cols[j]] + [c+1 for c in cols[j:]]
+        cols = cols[:j] + [cols[j]] + [c + 1 for c in cols[j:]]
 
     dist = 0
     for g1 in galaxies:
         for g2 in galaxies:
-            dist += abs(g1[0]-g2[0]) + abs(g1[1]-g2[1])
+            dist += abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
 
-    ic(int(dist/2))
+    ic(int(dist / 2))
 
-    return int(dist/2)
+    return int(dist / 2)
 
 
 # Part 2
 def part2(fname):
+    if real:
+        grow = 1000000-1
+    else:
+        grow = 100-1
+
     res = read_input(fname)
 
     universe = np.array([list(line) for line in res])
@@ -82,52 +85,61 @@ def part2(fname):
     galaxies = []
     for i in range(0, len(universe)):
         for j in range(0, len(universe[0])):
-            if universe[i,j] == '#':
-                galaxies.append([i,j])
+            if universe[i, j] == '#':
+                galaxies.append([i, j])
 
     # Find rows and columns containing only '.'
     rows = []
     cols = []
     for i in range(0, len(universe)):
-        if np.all(universe[i,:] == '.'):
-            rows.append(i)
+        if np.all(universe[i, :] == '.'):
+            rows.append([i, i])
     for j in range(0, len(universe[0])):
-        if np.all(universe[:,j] == '.'):
-            cols.append(j)
+        if np.all(universe[:, j] == '.'):
+            cols.append([j, j])
 
     ic(galaxies, rows, cols)
 
-    for size in range(1000000):
-        for i in range(len(rows) - 1, -1, -1):
-            # Shift all galaxies down
-            for galaxy in galaxies:
-                if galaxy[0] > rows[i]:
-                    galaxy[0] += 1
-            rows = rows[:i] + [rows[i]] + [r + 1 for r in rows[i:]]
+    for i in range(len(rows) - 1, -1, -1):
+        # Shift all galaxies 1 M down
+        for galaxy in galaxies:
+            if galaxy[0] > rows[i][0]:
+                galaxy[0] += grow
+        for idx in range(len(rows)):
+            if i == idx:
+                rows[i][1] += grow
+            elif idx > i:
+                rows[idx][0] += grow
+                rows[idx][1] += grow
 
-        for j in range(len(cols) - 1, -1, -1):
-            # Shift all galaxies right
-            for galaxy in galaxies:
-                if galaxy[1] > cols[j]:
-                    galaxy[1] += 1
-            cols = cols[:j] + [cols[j]] + [c + 1 for c in cols[j:]]
+    for j in range(len(cols) - 1, -1, -1):
+        # Shift all galaxies 1 M right
+        for galaxy in galaxies:
+            if galaxy[1] > cols[j][0]:
+                galaxy[1] += grow
+        for idx in range(len(cols)):
+            if j == idx:
+                cols[i][1] += grow
+            elif idx > j > idx:
+                cols[idx][0] += grow
+                cols[idx][1] += grow
 
     dist = 0
     for g1 in galaxies:
         for g2 in galaxies:
-            dist += abs(g1[0]-g2[0]) + abs(g1[1]-g2[1])
+            dist += abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
 
-    ic(int(dist/2))
+    ic(int(dist / 2))
 
-    return int(dist/2)
+    return int(dist / 2)
 
 
-real = False
+real = True
 verbose = True
 part = 2
 
-def main():
 
+def main():
     if verbose:
         ic.enable()
     else:
@@ -152,6 +164,7 @@ def main():
     toc = time.perf_counter()
     print(f"Time elapsed: {toc - tic:0.4f} seconds")
     return
+
 
 if __name__ == "__main__":
     main()
