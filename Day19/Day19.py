918536f4963ac38f624466c17b14c0fd34e07fd8
Bart Driessen <bart.driessen@gmail.com>
2024-11-10 22:35:56 +0100

    Day 19. Initial program.

--- Day19/Day19.py
+++ Day19/Day19.py
@@ -18,29 +18,79 @@ import copy
 def read_input(fn):
 
 
-    path = []
-    color = []
+    parts = []
+    workflows = []
     with open(fn) as f:
         lines = f.readlines()
 
 
     lines = [line.strip() for line in lines]
+
     # Split the line in tokens
     for line in lines:
-        # Split the line in tokens seperated by space
-        tokens = line.split(" ")
-        path.append([tokens[0], int(tokens[1])])
-        color.append(tokens[2][1:-1])
-    ic(path, color)
+        if line != "" and line[0] != '{':
+            # The label is the text until the first { character
+            label = line.split("{")[0]
+            # WorkfloWs is the text between the { and the }
+            workflow = line.split("{")[1].split("}")[0]
+
+            # split the line in tokens seperated by comma
+            tokens = workflow.split(",")
+            # Split token on : to get condition and action
+            for i in range(len(tokens)):
+                tokens[i] = tokens[i].split(":")
+            workflows.append([label, tokens])
+
+        elif line != "":
+            exec_line = line.split("{")[1].split("}")[0]
+            # change , to ; to make it a valid python line
+            exec_line = exec_line.replace(",", ";")
+            exec(exec_line, globals())
+            part = {'x':x, 'm':m, 'a':a, 's':s}
+
+            parts.append(part)
+
+    ic(workflows, parts)
 
 
 
-    return path, color
+    return workflows, parts
 
 
-def solve1(path, color):
-    res = 0
-    return res
+def evaluate(workflows, part):
+    # Evaluate the workflow
+    x = part['x']
+    m = part['m']
+    a = part['a']
+    s = part['s']
+
+
+    label = 'in'
+    ready = False
+    while not ready:
+        actions = workflows[label]
+        for i in range(len(actions)-1):
+            ic(actions[i])
+            fires = eval(actions[i][0])
+            ic(fires)
+            if fires:
+                new_label = actions[i]
+                break
+        new_label =
+    return actions[-1]
+
+
+def solve1(workflows, parts):
+
+    for part in parts:
+        evaluation_finished = False
+        while not evaluation_finished:
+            res = evaluate(workflows, part)
+            if res == 'A' or res == 'R':
+                evaluation_finished = True
+        ic(part, res)
+
+    return 0
 
 
 def score():
@@ -59,6 +109,8 @@ def solve2(path, codes):
 def part1(fname):
     res = 0
     workflows, parts = read_input(fname)
+    # Convert workflows to a dictionary
+    workflows = dict(workflows)
     res = solve1(workflows, parts)
     return res
 
